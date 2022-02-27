ea4b448d0b9c4b27a0f28b81fc1c78b7a0776ffd
Arthur0Rocha <ar_d_rocha@hotmail.com>
2022-02-27 07:45:45 -0300

    updated PAE header

--- components/pae/header.tsx
+++ components/pae/header.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react';
 import Link from 'next/link'
 
+import genStyles from '../../styles/general.module.css'
 import styles from '../../styles/PAE-Header.module.css';
 
 type Props = {
@@ -19,20 +20,29 @@ const pages = [
 function Header({ pageSlug } : Props) {
     return (
         <header className={styles.header}>
-            <ul className={styles.ul}>
+            <div className={styles.container}>
 
-            { pages.map( function( [slug, name, link] ) {
-                return (
-                    <Link key={slug} href={link}>
-                        <a>
-                            <li className={`${styles.li} ${slug===pageSlug ? styles.highlight : ''}`}>
-                                { name }
-                            </li>
-                        </a>
-                    </Link>
-                )
-            }) }
-            </ul>
+                <div className={styles.logo}>
+                    <h2>
+                        Arthur Rocha
+                    </h2>
+                    <p>Software & Engenharia</p>
+                </div>
+
+                <ul className={styles.ul}>
+                    { pages.map( function( [slug, name, link] ) {
+                        return (
+                            <Link key={slug} href={link}>
+                                <a>
+                                    <li className={`${styles.li} ${slug===pageSlug ? styles.highlight : ''}`}>
+                                        { name }
+                                    </li>
+                                </a>
+                            </Link>
+                        )
+                }) }
+                </ul>
+            </div>
         </header>
     )
 }
